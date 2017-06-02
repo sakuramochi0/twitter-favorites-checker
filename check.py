8aac0e6fbc8da7ade5fbd38f3b431ac87bff42d3
sakuramochi <sakuramochi.mochi.0@gmail.com>
2017-06-03 01:44:28 +0900

    add a feature to notice new followings

--- check.py
+++ check.py
@@ -5,16 +5,25 @@ import tweepy
 from get_tweepy import get_api
 
 filename = 'ids.yaml'
-user_id = 870250103004577792 # @love_mix_george
+user_id = 870250103004577792 # @xxxx_xxx_george ネタバレアカウント(一部伏せ字)
 api = get_api('sakuramochi_pre')
 
 def main():
-    ids = read_ids()
+    data = read_data()
+
+    # check the update of favorites
     ts = api.favorites(user_id=user_id, count=200)
     for t in ts:
-        if t.id not in ids:
-            notice(t)
-            add_id(t.id)
+        if t.id not in data['favorites']:
+            notice_tweet(t)
+            add_id('favorites', t.id)
+
+    # check the update of follows
+    us = api.friends(user_id=user_id, count=200)
+    for u in us:
+        if u.id not in data['follows']:
+            notice_follow(u)
+            add_id('follows', u.id)
 
 def get_url(t):
     return 'https://twitter.com/{}/status/{}'.format(
@@ -22,13 +31,20 @@ def get_url(t):
         t.id
     )
     
-def notice(t):
+def notice_tweet(t):
     status = '@skrmch_rhythpri {}'.format(get_url(t))
     api.update_status(status=status)
 
-def read_ids():
+def notice_follow(u):
+    status = '@skrmch_rhythpri https://twitter.com/{}'.format(u.screen_name)
+    api.update_status(status=status)
+    
+def read_data():
     if not os.path.exists(filename):
-        data = set()
+        data = dict(
+            favorites = set(),
+            follows = set(),
+        )
         with open(filename, 'w') as f:
             yaml.dump(data, f)
     else:
@@ -36,11 +52,14 @@ def read_ids():
             data = yaml.load(f)
     return data
 
-def add_id(id):
-    ids = read_ids()
-    ids.add(id)
+def write_data(data):
     with open(filename, 'w') as f:
-        yaml.dump(ids, f)
+        yaml.dump(data, f)
+
+def add_id(key, id):
+    data = read_data()
+    data[key].add(id)
+    write_data(data)
         
 if __name__ == '__main__':
     main()
